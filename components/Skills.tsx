36c87a2fb6b9dd1a3dbf0589807408123e1ae2f9
Virendra-Nawkar <virendranawkar1@gmail.com>
2026-04-24 09:47:32 +0000

    fix azure icon name

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react-icons/fa'
 import {
   SiKubernetes,
-  SiMicrosoftazure,
+  SiAzure,
   SiJenkins,
   SiGithubactions,
   SiTerraform,
@@ -34,7 +34,7 @@ const iconMap: Record<string, React.ComponentType<{ size?: number; className?: s
   FaGitAlt: FaGitAlt,
   FaPython: FaPython,
   SiKubernetes: SiKubernetes,
-  SiMicrosoftazure: SiMicrosoftazure,
+  SiAzure: SiAzure,
   SiJenkins: SiJenkins,
   SiGithubactions: SiGithubactions,
   SiTerraform: SiTerraform,
